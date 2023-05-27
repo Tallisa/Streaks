51da5108b601186d088b599ee3f8027260b20f81
MrSupiri <isalap99@gmail.com>
2023-05-27 14:03:57 +0000

    Updated Icons

--- src/AddTask.svelte
+++ src/AddTask.svelte
@@ -1,5 +1,9 @@
 <script>
   export let addTask;
+  const onAddTask = () => {
+    addTask(task);
+    task = ""
+  }
   let task = "";
 </script>
 
@@ -36,7 +40,7 @@
       placeholder="Enter a new task"
     />
     <button
-      on:click={() => addTask(task)}
+      on:click={() => onAddTask()}
       class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >Add task</button
     >
